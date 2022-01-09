5355090c5570988ac56b3a89412c33c75342dec5
Karina Macancela <karymacancela@gmail.com>
2022-01-09 15:06:37 -0500

    adding material ui and topbar items

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,29 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import {Person, Mail} from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className="topbar">
-            Top bar!
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">karina macancela.</a>
+                    <div className="item">
+                        <Person className="icon" />
+                        <span>+1 347 570 5804</span>
+                    </div>
+                    <div className="item">
+                        <Mail className="icon" />
+                        <span>karymacancela@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
